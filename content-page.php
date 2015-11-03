dd21a4a2bed6e63634cbe77a507db7d5988e1365
msm <msm2e4d534d@gmail.com>
2015-11-03 14:24:59 -0500

    Remove title from pages

--- content-page.php
+++ content-page.php
@@ -10,10 +10,6 @@
 
 <div class="post-inner-content">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header page-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
